244c25fd6751485dfec7f3bb6a16ce520dc9dce3
Harshika-coder <chaurasiaharshika2612@Gmail.com>
2025-10-17 22:45:18 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputTask = document.getElementById("taskInput");
+const listContainer = document.getElementById("taskList");
+
+
+function addTask(){
+    if(inputTask.value === ''){
+        alert("You must write something!");
+    } else{
+        let li = document.createElement("li");
+    }
+}   
